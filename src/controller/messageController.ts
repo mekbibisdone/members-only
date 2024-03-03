a3f9e8685c81b1a84fa286654f8822bc73c0cc5b
mekbibisdone <mekbibtare@gmail.com>
2024-03-03 11:21:41 +0300

    Allow admin to delete messages

--- src/controller/messageController.ts
+++ src/controller/messageController.ts
@@ -2,7 +2,7 @@ import { Temporal } from "@js-temporal/polyfill";
 import Message from "@src/models/messageModel";
 import { formatInstant } from "@src/util/misc";
 import { NextFunction, Response, Request } from "express";
-import { body, matchedData, validationResult } from "express-validator";
+import { body, matchedData, param, validationResult } from "express-validator";
 
 export const getHomePage = async (
   req: Request,
@@ -63,6 +63,37 @@ export const saveMessage = [
           await messageDocument.save();
           res.redirect("/");
         }
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+];
+
+export const deleteMessage = [
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user)
+      res.redirect("/login");
+    else if("isAdmin" in req.user && !req.user.isAdmin)
+      res.redirect("/");
+    else next();
+  },
+  param("id").trim().escape().notEmpty().withMessage("Message id is required"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        res.render("index", {
+          title: "Homepage",
+          user: req.user,
+          errors: result
+            .array()
+            .map((error) => ("msg" in error ? (error.msg as string) : null)),
+        });
+      } else {
+        const data = matchedData(req);
+        await Message.findByIdAndDelete(data.id);
+        res.redirect("/");
       }
     } catch (err) {
       next(err);
--- src/routes/messageRoute.ts
+++ src/routes/messageRoute.ts
@@ -1,9 +1,15 @@
-import { getHomePage, saveMessage } from "@src/controller/messageController";
+import {
+  deleteMessage,
+  getHomePage,
+  saveMessage,
+} from "@src/controller/messageController";
 import { Router } from "express";
 const messageRoute = Router();
 
 messageRoute.get("/", getHomePage);
 
-messageRoute.post("/message", saveMessage)
+messageRoute.post("/message", saveMessage);
+
+messageRoute.get("/message/:id/delete", deleteMessage);
 
 export default messageRoute;
